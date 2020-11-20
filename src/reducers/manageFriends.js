6ae0c95e176bd090d4eba1c4231e70cc25198afd
Kat Tochowicz <ktochow1@binghamton.edu>
2020-11-20 20:36:04 +0100

    commit tree bef94d6f5185d1b6d42c02f414c10a671e686a58

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,28 @@
-export function manageFriends(state, action){
-}
+export function manageFriends(state={friends: []}, action){
+    switch(action.type){
+        case 'ADD_FRIEND': {
+            return {
+                ...state,
+                friends: [
+                    ...state.friends,
+                    {hometown: action.friend.hometown,
+                    id: action.friend.id,
+                    name: action.friend.name}
+                ]
+            }
+        }
+        case 'REMOVE_FRIEND': {
+            let friendRemoval = state.friends.findIndex(friend => friend.id === action.id)
+            return {
+               ...state,
+               friends: [
+                   ...state.friends.slice(0, friendRemoval),
+                   ...state.friends.slice(friendRemoval+1)
+               ]
+            }
+        }
+        
+            default: return state    
+            }
+    }
+
